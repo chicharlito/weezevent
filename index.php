28230aa6797d65a1121a4e8f157d18b97de4430c
Charles Reinert <Charles Reinert>
2016-11-23 11:24:50 +0100

    Traduction de la login page

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if(isset($_POST['btn-login']))
 <div class="container">
      <div class="form-container">
         <form method="post">
-            <h2>Sign in.</h2><hr />
+            <h2>Se connecter</h2><hr />
             <?php
             if(isset($error))
             {
@@ -46,19 +46,19 @@ if(isset($_POST['btn-login']))
             }
             ?>
             <div class="form-group">
-             <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
+             <input type="text" class="form-control" name="txt_uname_email" placeholder="Pseudo ou adresse mail" required />
             </div>
             <div class="form-group">
-             <input type="password" class="form-control" name="txt_password" placeholder="Your Password" required />
+             <input type="password" class="form-control" name="txt_password" placeholder="Votre mot de passe" required />
             </div>
             <div class="clearfix"></div><hr />
             <div class="form-group">
              <button type="submit" name="btn-login" class="btn btn-block btn-primary">
-                 <i class="glyphicon glyphicon-log-in"></i>&nbsp;SIGN IN
+                 <i class="glyphicon glyphicon-log-in"></i>&nbsp;Se connecter
                 </button>
             </div>
             <br />
-            <label>Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
+            <label>Vous n'avez pas encore de compte ? <a href="sign-up.php">Créez un compte</a></label>
         </form>
        </div>
 </div>
