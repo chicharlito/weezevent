4b3cb818683cbfc85519772eb40382a94fcb1f89
chicharlito <r-c95@hotmail.fr>
2016-11-23 11:17:50 +0100

    Passage au nouveau système CodingCage

--- index.php
+++ index.php
@@ -1,83 +1,67 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Page Login</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	</head>
-	<body>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-md-offset-3">
-					<div class="panel panel-login">
-						<div class="panel-heading">
-							<div class="row">
-								<div class="col-xs-6">
-									<a href="#" class="active" id="login-form-link">Login</a>
-								</div>
-								<div class="col-xs-6">
-									<a href="#" id="register-form-link">S'inscrire</a>
-								</div>
-							</div>
-							<hr>
-						</div>
-						<div class="panel-body">
-							<div class="row">
-								<div class="col-lg-12">
-									<form id="login-form" action="http://phpoll.com/login/process" method="post" role="form" style="display: block;">
-										<div class="form-group">
-											<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Pseudo" value="">
-										</div>
-										<div class="form-group">
-											<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Mot de passe">
-										</div>
-										<div class="form-group text-center">
-											<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-											<label for="remember"> Se souvenir de moi</label>
-										</div>
-										<div class="form-group">
-											<div class="row">
-												<div class="col-sm-6 col-sm-offset-3">
-													<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Connexion">
-												</div>
-											</div>
-										</div>
-										<div class="form-group">
-											<div class="row">
-												<div class="col-lg-12">
-													<div class="text-center">
-														<a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Mot de passe oublié ?</a>
-													</div>
-												</div>
-											</div>
-										</div>
-									</form>
-									<form id="register-form" action="http://phpoll.com/register/process" method="post" role="form" style="display: none;">
-										<div class="form-group">
-											<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Pseudo" value="">
-										</div>
-										<div class="form-group">
-											<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Adresse mail" value="">
-										</div>
-										<div class="form-group">
-											<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Mot de passe">
-										</div>
-										<div class="form-group">
-											<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirmez votre mot de passe">
-										</div>
-										<div class="form-group">
-											<div class="row">
-												<div class="col-sm-6 col-sm-offset-3">
-													<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="S'inscrire">
-												</div>
-											</div>
-										</div>
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</body>
+<?php
+require_once 'dbconfig.php';
+
+if($user->is_loggedin()!="")
+{
+ $user->redirect('home.php');
+}
+
+if(isset($_POST['btn-login']))
+{
+ $uname = $_POST['txt_uname_email'];
+ $umail = $_POST['txt_uname_email'];
+ $upass = $_POST['txt_password'];
+  
+ if($user->login($uname,$umail,$upass))
+ {
+  $user->redirect('home.php');
+ }
+ else
+ {
+  $error = "Wrong Details !";
+ } 
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login : cleartuts</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css"  />
+</head>
+<body>
+<div class="container">
+     <div class="form-container">
+        <form method="post">
+            <h2>Sign in.</h2><hr />
+            <?php
+            if(isset($error))
+            {
+                  ?>
+                  <div class="alert alert-danger">
+                      <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
+                  </div>
+                  <?php
+            }
+            ?>
+            <div class="form-group">
+             <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
+            </div>
+            <div class="form-group">
+             <input type="password" class="form-control" name="txt_password" placeholder="Your Password" required />
+            </div>
+            <div class="clearfix"></div><hr />
+            <div class="form-group">
+             <button type="submit" name="btn-login" class="btn btn-block btn-primary">
+                 <i class="glyphicon glyphicon-log-in"></i>&nbsp;SIGN IN
+                </button>
+            </div>
+            <br />
+            <label>Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
+        </form>
+       </div>
+</div>
+
+</body>
 </html>
